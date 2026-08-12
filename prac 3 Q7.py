59046ef685783d718770c941218b9d9e508ca4fb
Prasad@123 <prasadposam09@gmail.com>
2026-08-12 11:20:27 +0530

    Add files via upload

---
+++ prac 3 Q7.py
@@ -0,0 +1,26 @@
+num1 = float(input("Enter First number:"))
+num2 = float(input("Enter Second number:"))
+operator = input("Enter operator(+, -, *, /):")
+
+if operator == "+":
+    print("Result =", num1 + num2)
+elif operator == "-":
+    print("Result =", num1 - num2)
+elif operator == "*":
+    print("result =", num1 * num2)
+elif operator == "/":
+    if num2 ! = 0:
+        print("Result = ", num1 / num2)
+        else 
+
+
+
+
+
+
+
+
+
+
+
+        
